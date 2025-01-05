fa472d4221928c7d9c7b1130364a32de21f65d47
Glen De Cauwsemaecker <glen@plabayo.tech>
2025-01-05 15:05:37 +0100

    Update rama.rb to 0.2.0-alpha.5

--- Formula/rama.rb
+++ Formula/rama.rb
@@ -4,9 +4,9 @@
 class Rama < Formula
     desc "move and transform network packets with rama"
     homepage "https://ramaproxy.org"
-    url "https://github.com/plabayo/rama/releases/download/0.2.0-alpha.4/rama-aarch64-apple-darwin.tar.gz"
-    sha256 "201d1d414fe41211dc800c7d89c6bf71ab0c481c4cb0777c30a44bec07052d7e"
-    version "0.2.0-alpha.4"
+    url "https://github.com/plabayo/rama/releases/download/0.2.0-alpha.5/rama-aarch64-apple-darwin.tar.gz"
+    sha256 "4c14b7144b95601e22c97604364ab53774a62ab897c7be560dd050ceb1f2cab6"
+    version "0.2.0-alpha.5"
   
     def install
       bin.install "rama"
