fc02121068ae20161177305fdfc59a959b331086
Glen De Cauwsemaecker <glen@plabayo.tech>
2025-04-14 13:58:35 +0200

    Update rama.rb to 0.2.0-alpha.8

--- Formula/rama.rb
+++ Formula/rama.rb
@@ -4,9 +4,9 @@
 class Rama < Formula
     desc "move and transform network packets with rama"
     homepage "https://ramaproxy.org"
-    url "https://github.com/plabayo/rama/releases/download/0.2.0-alpha.6/rama-aarch64-apple-darwin.tar.gz"
-    sha256 "6648befc960dc670b3342adb6f621f18993e05c27c95efcb47f53519cae26ce3"
-    version "0.2.0-alpha.6"
+    url "https://github.com/plabayo/rama/releases/download/0.2.0-alpha.8/rama.aarch64-apple-darwin.tar.xz"
+    # sha256 "6648befc960dc670b3342adb6f621f18993e05c27c95efcb47f53519cae26ce3"
+    version "0.2.0-alpha.8"
   
     def install
       bin.install "rama"
