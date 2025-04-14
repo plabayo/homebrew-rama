d3ad3b51ef6c60b773894555162ad15e39f05e2a
Glen De Cauwsemaecker <glen@plabayo.tech>
2025-04-14 21:31:23 +0200

    Update rama.rb add checksum

--- Formula/rama.rb
+++ Formula/rama.rb
@@ -5,7 +5,7 @@ class Rama < Formula
     desc "move and transform network packets with rama"
     homepage "https://ramaproxy.org"
     url "https://github.com/plabayo/rama/releases/download/0.2.0-alpha.8/rama.aarch64-apple-darwin.tar.xz"
-    # sha256 "6648befc960dc670b3342adb6f621f18993e05c27c95efcb47f53519cae26ce3"
+    sha256 "1180dcbaac0ea7df6a537455b14286677d19364b9eb72a98952e3854803920f7"
     version "0.2.0-alpha.8"
   
     def install
