029c13eaa6170c93a21eff606dc6929de7f51518
Glen De Cauwsemaecker <glen@plabayo.tech>
2025-08-05 20:05:49 +0200

    Update rama.rb: bump version to 0.3.0

--- Formula/rama.rb
+++ Formula/rama.rb
@@ -6,7 +6,7 @@ class Rama < Formula
     homepage "https://ramaproxy.org"
     url "https://github.com/plabayo/rama/releases/download/0.3.0-alpha.2/rama.aarch64-apple-darwin.tar.xz"
     sha256 "1d9fd1ab28e916d15d9ece04c5280136bc477d750a9ecd90b837173ff43f2c23"
-    version "0.2.0"
+    version "0.3.0"
   
     def install
       bin.install "rama"
